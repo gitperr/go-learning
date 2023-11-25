641c60d1d0a0bbd7f09ee5927f67772492f4df51
Alper Polat <gitperr@gmail.com>
2023-11-25 19:16:55 +0200

    Test signoff

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -59,7 +59,7 @@ func main() {
 		i++
 		fmt.Println("Question:", question)
 		go func() {
-			var answer string
+			var answer strings
 			fmt.Println("Your answer: ")
 			fmt.Scanln(&answer)
 			answerChannel <- answer
